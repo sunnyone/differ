1e3e43ffc495837fe8f9ad2b095aa1b87aa45851
Vaclav Rosecky <xrosecky@gmail.com>
2013-10-14 18:35:12 +0200

    create temporary files using standard Java API

--- differ-webapp/src/main/java/cz/nkp/differ/gui/components/UploadReceiver.java
+++ differ-webapp/src/main/java/cz/nkp/differ/gui/components/UploadReceiver.java
@@ -3,8 +3,8 @@ package cz.nkp.differ.gui.components;
 import com.vaadin.ui.Upload.Receiver;
 import cz.nkp.differ.DifferApplication;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 
 /**
@@ -13,37 +13,37 @@ import java.io.OutputStream;
  */
 public class UploadReceiver implements Receiver {
     
-    private static String tempDir = "/tmp/differ/anon_uploads/";
-    private String filePrefix;
+    private static final String filePrefix = "differ_webapp";
+    
     private File file;
-    private static int i;
     
     public UploadReceiver() {
-        this.filePrefix = "image" + (++i) + "-";
     }
  
     @Override
     public OutputStream receiveUpload(String filename, String mimeType) {           
-           FileOutputStream fos;
            try {
-               File dir = new File(tempDir);
-               if (!dir.exists()) {
-                   dir.mkdirs();
-               }
-               //guarantee filename is unique before writing to stream
-               while ((file = new File(tempDir + filePrefix + filename)).exists()) {
-                   filePrefix = "image" + (++i) + "-";
-               }
-               fos = new FileOutputStream(file);
-           } catch (FileNotFoundException io) {
+               String fileSuffix = "." + getExtension(filename);
+               file = File.createTempFile(filePrefix, fileSuffix);
+               return new FileOutputStream(file);
+           } catch (IOException ioe) {
                DifferApplication.getCurrentApplication().getMainWindow().showNotification("Error", "<br/>Error while uploading file");
                return null;
            }
-           return fos;
     }
-      
+
     public File getFile() {
         return file;
     }
+    
+    private static String getExtension(String fileName) {
+	String extension = "";
+	int dotAt = fileName.lastIndexOf('.');
+	if (dotAt != -1) {
+	    extension = fileName.substring(dotAt + 1);
+	}
+	return extension;
+    }
+
     
 }
