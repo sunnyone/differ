4cd32ab12f191e46de075aba0ea738eee0141ecd
Vaclav Rosecky <xrosecky@gmail.com>
2013-10-15 18:20:18 +0200

    class for monitoring execution time added

---
+++ differ-common/src/main/java/cz/nkp/differ/tools/ExecutionTime.java
@@ -0,0 +1,23 @@
+package cz.nkp.differ.tools;
+
+/**
+ *
+ * @author xrosecky
+ */
+public class ExecutionTime {
+
+    private long start = System.currentTimeMillis();
+    
+    public ExecutionTime() {
+    }
+    
+    public void reset() {
+        long start = System.currentTimeMillis();
+    }
+    
+    public long getTime() {
+        long now = System.currentTimeMillis();
+        return (now - start);
+    }
+    
+}
