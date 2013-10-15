2a1cde192e2c453d6f4c5008001e36c9137479c9
Vaclav Rosecky <xrosecky@gmail.com>
2013-10-15 20:18:01 +0200

    tests fixed for new Progress listener API

---
+++ differ-webapp/src/main/java/cz/nkp/differ/gui/components/WebProgressListener.java
@@ -0,0 +1,9 @@
+package cz.nkp.differ.gui.components;
+
+import cz.nkp.differ.listener.ProgressListener;
+
+public interface WebProgressListener extends ProgressListener {
+
+    public void ready(Object c);
+    
+}
