16f402a62db735daba14ac8f12b0aad4a1e83fba
Vaclav Rosecky <xrosecky@gmail.com>
2013-10-27 16:57:46 +0100

    LoginUserWindow cleanup

--- differ-webapp/src/main/java/cz/nkp/differ/gui/windows/LoginUserWindow.java
+++ differ-webapp/src/main/java/cz/nkp/differ/gui/windows/LoginUserWindow.java
@@ -8,75 +8,72 @@ import com.vaadin.ui.Layout;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Window;
 import cz.nkp.differ.DifferApplication;
 import cz.nkp.differ.gui.components.ProjectHeaderPanel;
 import cz.nkp.differ.gui.tabs.DifferProgramTab;
 import cz.nkp.differ.util.GUIMacros;
 
 /**
- * 
+ *
  * @author Thomas Truax
  */
-public class LoginUserWindow extends Window implements ClickListener {
-    
+public class LoginUserWindow extends CustomWindow {
+
     DifferProgramTab appBody;
     ProjectHeaderPanel appHeader;
     TextField nameField;
-    PasswordField passField;   
-    
+    PasswordField passField;
+
+    private class LoginButtonClickListener implements ClickListener {
+
+        @Override
+        public void buttonClick(Button.ClickEvent event) {
+
+            String nameValue = (String) nameField.getValue();
+            String passValue = (String) passField.getValue();
+
+            if (nameValue != null && passValue != null) {
+                if (appBody.login(nameValue, passValue)) {
+                    appHeader.setLoggedIn(nameValue);
+                    DifferApplication.getCurrentApplication().getMainWindow().showNotification("Success", "<br/>You are now logged in as " + nameValue);
+                    GUIMacros.closeWindow(LoginUserWindow.this);
+                }
+            }
+        }
+    }
+
     public LoginUserWindow(DifferProgramTab appBody, ProjectHeaderPanel appHeader) {
-        setCaption("Login");
-	setModal(true);
-	setDraggable(false);
-	setResizable(false);
-	center();
-	setWidth("25%");
+        super("Login");
         this.appBody = appBody;
         this.appHeader = appHeader;
-        
+
         addComponent(createLoginUserWindow());
-        
+
         HorizontalLayout buttonLayout = new HorizontalLayout();
-        
+
         Button login = new Button("Login");
-	login.addListener(this);
-	buttonLayout.addComponent(login);
+        login.addListener(new LoginButtonClickListener());
+        buttonLayout.addComponent(login);
 
-	Button close = new Button("Close");
-	close.addListener(GUIMacros.createWindowCloseButtonListener(this));
-	buttonLayout.addComponent(close);
+        Button close = new Button("Close");
+        close.addListener(GUIMacros.createWindowCloseButtonListener(this));
+        buttonLayout.addComponent(close);
 
-	addComponent(buttonLayout);
+        addComponent(buttonLayout);
     }
-    
+
     private Layout createLoginUserWindow() {
-        
+
         VerticalLayout layout = new VerticalLayout();
 
-	nameField = new TextField("Username");
-	nameField.addValidator(new NullValidator("You must provide a username!", false));
-	layout.addComponent(nameField);
+        nameField = new TextField("Username");
+        nameField.addValidator(new NullValidator("You must provide a username!", false));
+        layout.addComponent(nameField);
 
-	passField = new PasswordField("Password");
-	passField.addValidator(new NullValidator("You must provide a password!", false));
-	layout.addComponent(passField);
+        passField = new PasswordField("Password");
+        passField.addValidator(new NullValidator("You must provide a password!", false));
+        layout.addComponent(passField);
 
-	return layout;
-    }
-
-    @Override
-    public void buttonClick(Button.ClickEvent event) {
-        
-	String nameValue = (String) nameField.getValue();
-	String passValue = (String) passField.getValue();
-
-        if (nameValue != null && passValue != null) {
-            if (appBody.login(nameValue, passValue)) {
-                appHeader.setLoggedIn(nameValue);
-                DifferApplication.getCurrentApplication().getMainWindow().showNotification("Success", "<br/>You are now logged in as " + nameValue);
-                GUIMacros.closeWindow(LoginUserWindow.this);
-            }               
-        }
+        return layout;
     }
 }
