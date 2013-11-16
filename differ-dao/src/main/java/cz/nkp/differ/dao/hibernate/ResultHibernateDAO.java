6a82bb4463ac367cf0d58582478a15a271ecfd5b
Vaclav Rosecky <xrosecky@gmail.com>
2013-11-16 20:22:21 +0100

    ResultDAO class and implementation

---
+++ differ-dao/src/main/java/cz/nkp/differ/dao/hibernate/ResultHibernateDAO.java
@@ -0,0 +1,24 @@
+package cz.nkp.differ.dao.hibernate;
+
+import cz.nkp.differ.dao.ResultDAO;
+import cz.nkp.differ.model.Result;
+import cz.nkp.differ.model.User;
+import java.util.List;
+
+/**
+ *
+ * @author xrosecky
+ */
+public class ResultHibernateDAO extends GenericHibernateDAO<Result, Long> implements ResultDAO  {
+
+    @Override
+    public List<Result> findAllShared() {
+	return (List<Result>) getHibernateTemplate().find("from Result where shared = true");
+    }
+
+    @Override
+    public List<Result> findByUser(User user) {
+	return getHibernateTemplate().find("from Result where userId = ?", user.getId());
+    }
+
+}
