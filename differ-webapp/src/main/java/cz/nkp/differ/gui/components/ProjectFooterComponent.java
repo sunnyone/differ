df5382c0faf2fec9cec75fd818ec18822b6b18c1
Vaclav Rosecky <xrosecky@gmail.com>
2013-11-19 21:14:07 +0100

    images in footer are clickable

--- differ-webapp/src/main/java/cz/nkp/differ/gui/components/ProjectFooterComponent.java
+++ differ-webapp/src/main/java/cz/nkp/differ/gui/components/ProjectFooterComponent.java
@@ -1,5 +1,6 @@
 package cz.nkp.differ.gui.components;
 
+import com.vaadin.terminal.ExternalResource;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Alignment;
@@ -8,8 +9,10 @@ import com.vaadin.ui.Embedded;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Layout;
+import com.vaadin.ui.Link;
 import com.vaadin.ui.Panel;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Generates the footer of the main application window, adding banners/logos/text as necessary.
@@ -30,14 +33,24 @@ public class ProjectFooterComponent extends CustomComponent {
     }
     
     private Layout createFooterLayout() {
-        ArrayList<Embedded> embedded = new ArrayList<Embedded>();
-        embedded.add(new Embedded(null, new ThemeResource("img/LOGO_Ministerstvo_Kultury.png")));
-        embedded.add(new Embedded(null, new ThemeResource("img/LOGO_gsoc_2013.png")));
-        embedded.add(new Embedded(null, new ThemeResource("img/LOGO_nkp_en.png")));
-        GridLayout layout = new GridLayout(embedded.size(), 1);
+        List<Link> links = new ArrayList<Link>();
+        
+        Link mk = new Link("", new ExternalResource("http://www.mkcr.cz/"));
+        mk.setIcon(new ThemeResource("img/LOGO_Ministerstvo_Kultury.png"));
+        links.add(mk);
+        
+        Link gsoc = new Link("", new ExternalResource("https://developers.google.com/open-source/soc/"));
+        gsoc.setIcon(new ThemeResource("img/LOGO_gsoc_2013.png"));
+        links.add(gsoc);
+        
+        Link nkp = new Link("", new ExternalResource("http://www.nkp.cz/"));
+        nkp.setIcon(new ThemeResource("img/LOGO_nkp_en.png"));
+        links.add(nkp);
+        
+        GridLayout layout = new GridLayout(links.size(), 1);
         layout.addStyleName("v-footer-logos");
-        for (Embedded img : embedded) {
-            layout.addComponent(img);
+        for (Link link : links) {
+            layout.addComponent(link);
         }
         return layout;
     }
