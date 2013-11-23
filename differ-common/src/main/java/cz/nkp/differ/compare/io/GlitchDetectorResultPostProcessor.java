31d71ae2c7f1589baa56077ee7eea19cbee8ed71
Vaclav Rosecky <xrosecky@gmail.com>
2013-11-23 16:03:43 +0100

    configurable glitch detector by user

--- differ-common/src/main/java/cz/nkp/differ/compare/io/GlitchDetectorResultPostProcessor.java
+++ differ-common/src/main/java/cz/nkp/differ/compare/io/GlitchDetectorResultPostProcessor.java
@@ -2,25 +2,29 @@ package cz.nkp.differ.compare.io;
 
 import cz.nkp.differ.compare.metadata.ImageMetadata;
 import cz.nkp.differ.compare.metadata.MetadataSource;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  *
  * @author xrosecky
  */
-public class GlitchDetectorResultPostProcessor implements ResultPostProcessor {
+public class GlitchDetectorResultPostProcessor implements ResultPostProcessor, RefreshableResultPostProcessor<GlitchDetectorConfig> {
     
     public static final String SOURCE_NAME = "Glitch detector";
 
     private static final MetadataSource source = new MetadataSource(SOURCE_NAME);
     
     private static final String[] colours = new String[]{ "Max red ratio", "Max green ratio", "Max blue ratio" };
-    
-    private double maxAllowedRatio = 0.6;
-    
+
+    private GlitchDetectorConfig config = new GlitchDetectorConfig();
+
     @Override
-    public void process(ImageProcessorResult result) {
-        if (result.getType().equals(ImageProcessorResult.Type.COMPARISON)) {
-            return;
+    public List<ImageMetadata> process(ImageProcessorResult result, GlitchDetectorConfig glitchDetectorConfig) {
+	List<ImageMetadata> metadata = new ArrayList<ImageMetadata>();
+	if (result.getType().equals(ImageProcessorResult.Type.COMPARISON)) {
+            return Collections.emptyList();
         }
         boolean glitchDetected = false;
         int resolution = result.getHeight() * result.getWidth();
@@ -30,16 +34,22 @@ public class GlitchDetectorResultPostProcessor implements ResultPostProcessor {
                 glitchDetected = true;
             }
 	    ImageMetadata white = createImageMetadataEntry("Number of absolute white pixels", Integer.toString(grayscaleHistogram[0]));
-	    result.getMetadata().add(white);
+	    metadata.add(white);
 	    ImageMetadata black = createImageMetadataEntry("Number of absolute black pixels", Integer.toString(grayscaleHistogram[255]));
-	    result.getMetadata().add(black);
+	    metadata.add(black);
         }
         int[][] colorHistogram = result.getHistogram();
         if (colorHistogram != null) {
             for (int i = 0; i != 3; i++) {
                 double ratio = colorHistogram[i][255] / (resolution * 1.0);
 		ImageMetadata entry = createImageMetadataEntry(colours[i], Double.toString(ratio * 100));
-		result.getMetadata().add(entry);
+		metadata.add(entry);
+		double maxAllowedRatio = 1.0;
+		switch (i) {
+		    case 0: maxAllowedRatio = glitchDetectorConfig.getMaxAllowedRatioForAbsoluteRed(); break;
+		    case 1: maxAllowedRatio = glitchDetectorConfig.getMaxAllowedRatioForAbsoluteGreen(); break;
+		    case 2: maxAllowedRatio = glitchDetectorConfig.getMaxAllowedRatioForAbsoluteBlue(); break;
+		}
                 if (ratio > maxAllowedRatio) {
                     glitchDetected = true;
                 }
@@ -49,7 +59,21 @@ public class GlitchDetectorResultPostProcessor implements ResultPostProcessor {
         entry.setSource(source);
         entry.setKey("Glitch detected");
         entry.setValue(glitchDetected);
-        result.getMetadata().add(entry);
+	metadata.add(entry);
+	return metadata;
+    }
+
+    @Override
+    public void process(ImageProcessorResult result) {
+        result.getMetadata().addAll(process(result, config));
+    }
+
+    public GlitchDetectorConfig getConfig() {
+	return config;
+    }
+
+    public void setConfig(GlitchDetectorConfig config) {
+	this.config = config;
     }
     
     private ImageMetadata createImageMetadataEntry(String key, String value) {
