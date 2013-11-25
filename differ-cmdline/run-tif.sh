6659a1bb21a895e897db0684bdd6afec7dd4ba36
Jan Stavěl <stavel.jan@gmail.com>
2013-11-25 12:17:38 +0100

    changed some test script.

--- differ-cmdline/run-tif.sh
+++ differ-cmdline/run-tif.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-java -Dapplication.properties=file:./application.properties -jar target/differ-cmdline-0.0.1-SNAPSHOT.jar --save-raw-outputs --save-report --save-properties image.tif
-
+java -Dapplication.properties=file:./application.properties -jar target/differ-cmdline-0.0.1-SNAPSHOT.jar --save-raw-outputs --save-report --save-properties ../docs/images/1002186430_000015.tif
