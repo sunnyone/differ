09207eaa9bc8ce3986ecf04c4224353145ee6d51
Jan Stavěl <stavel.jan@gmail.com>
2014-01-10 03:45:05 +0100

    added more glossaries.

--- differ-common/src/main/java/cz/nkp/differ/tools/HTMLGlossaryUtil.java
+++ differ-common/src/main/java/cz/nkp/differ/tools/HTMLGlossaryUtil.java
@@ -25,43 +25,44 @@ import java.util.Scanner;
  *    en_US/phrase-1.html
  *          phrase-2.html
  *          phrase-3.html
+ *
+ *  Directory is located at resources dir.
  */
 
 public class HTMLGlossaryUtil implements GlossaryUtil {
-
-	public HTMLGlossaryUtil() {
-	}
+    public HTMLGlossaryUtil() {
+    }
     
-	/**
-	 * @see cz.nkp.differ.tools.GlossaryUtil#getGlossaryFor(java.lang.String, java.util.Locale)
-	 */
-	@Override
-	public String getGlossaryFor(String phrase, Locale locale) {
-		String normalized = this.normalize(phrase);
+    /**
+     * @see cz.nkp.differ.tools.GlossaryUtil#getGlossaryFor(java.lang.String, java.util.Locale)
+     */
+    @Override
+    public String getGlossaryFor(String phrase, Locale locale) {
+        String normalized = this.normalize(phrase);
         File directory = new File("/glossary",locale.toString());
         File glossaryPath = new File(directory,normalized + ".html");
-	    InputStream is = this.getClass().getResourceAsStream(glossaryPath.getPath());
-	    if( is == null){
-	    	return null;
-	    }
-	    Scanner scanner = new Scanner(is,"UTF-8");
-		String glossary = scanner.useDelimiter("\\A").next();
-		scanner.close();
-		try {
-			is.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return glossary.trim();
-	}
-
-	/**
-	 * 
-	 * @return normalized string
-	 */
-	public String normalize(String val){
-		String normalized = java.text.Normalizer.normalize(val, Normalizer.Form.NFD)
-				.replaceAll("[^\\p{ASCII}]", "").toLowerCase().trim().replaceAll("[\\ \\t]+","-");
-		return normalized;
-	}
+        InputStream is = this.getClass().getResourceAsStream(glossaryPath.getPath());
+        if( is == null){
+            return null;
+        }
+        Scanner scanner = new Scanner(is,"UTF-8");
+        String glossary = scanner.useDelimiter("\\A").next();
+        scanner.close();
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return glossary.trim();
+    }
+    
+    /**
+     * 
+     * @return normalized string
+     */
+    public String normalize(String val){
+        String normalized = java.text.Normalizer.normalize(val, Normalizer.Form.NFD)
+            .replaceAll("[^\\p{ASCII}]", "").toLowerCase().trim().replaceAll("[\\ \\t]+","-");
+        return normalized;
+    }
 }
