e30d53607fc194f05ef1373ae7083bf67bcfcc5b
Vaclav Rosecky <xrosecky@gmail.com>
2014-03-09 10:39:13 +0100

    default values for GlitchDetectorConfig are in percent

--- differ-common/src/main/java/cz/nkp/differ/compare/io/GlitchDetectorConfig.java
+++ differ-common/src/main/java/cz/nkp/differ/compare/io/GlitchDetectorConfig.java
@@ -1,39 +1,42 @@
 package cz.nkp.differ.compare.io;
 
 /**
- *
+ * 
  * @author xrosecky
  */
 public class GlitchDetectorConfig {
 
-    private double maxAllowedRatioForAbsoluteRed = 0.6;
+	private double maxAllowedRatioForAbsoluteRed = 60.0d;
 
-    private double maxAllowedRatioForAbsoluteGreen = 0.6;
+	private double maxAllowedRatioForAbsoluteGreen = 60.0d;
 
-    private double maxAllowedRatioForAbsoluteBlue = 0.6;
+	private double maxAllowedRatioForAbsoluteBlue = 60.0d;
 
-    public double getMaxAllowedRatioForAbsoluteBlue() {
-	return maxAllowedRatioForAbsoluteBlue;
-    }
+	public double getMaxAllowedRatioForAbsoluteBlue() {
+		return maxAllowedRatioForAbsoluteBlue;
+	}
 
-    public void setMaxAllowedRatioForAbsoluteBlue(double maxAllowedRatioForAbsoluteBlue) {
-	this.maxAllowedRatioForAbsoluteBlue = maxAllowedRatioForAbsoluteBlue;
-    }
+	public void setMaxAllowedRatioForAbsoluteBlue(
+			double maxAllowedRatioForAbsoluteBlue) {
+		this.maxAllowedRatioForAbsoluteBlue = maxAllowedRatioForAbsoluteBlue;
+	}
 
-    public double getMaxAllowedRatioForAbsoluteGreen() {
-	return maxAllowedRatioForAbsoluteGreen;
-    }
+	public double getMaxAllowedRatioForAbsoluteGreen() {
+		return maxAllowedRatioForAbsoluteGreen;
+	}
 
-    public void setMaxAllowedRatioForAbsoluteGreen(double maxAllowedRatioForAbsoluteGreen) {
-	this.maxAllowedRatioForAbsoluteGreen = maxAllowedRatioForAbsoluteGreen;
-    }
+	public void setMaxAllowedRatioForAbsoluteGreen(
+			double maxAllowedRatioForAbsoluteGreen) {
+		this.maxAllowedRatioForAbsoluteGreen = maxAllowedRatioForAbsoluteGreen;
+	}
 
-    public double getMaxAllowedRatioForAbsoluteRed() {
-	return maxAllowedRatioForAbsoluteRed;
-    }
+	public double getMaxAllowedRatioForAbsoluteRed() {
+		return maxAllowedRatioForAbsoluteRed;
+	}
 
-    public void setMaxAllowedRatioForAbsoluteRed(double maxAllowedRatioForAbsoluteRed) {
-	this.maxAllowedRatioForAbsoluteRed = maxAllowedRatioForAbsoluteRed;
-    }
+	public void setMaxAllowedRatioForAbsoluteRed(
+			double maxAllowedRatioForAbsoluteRed) {
+		this.maxAllowedRatioForAbsoluteRed = maxAllowedRatioForAbsoluteRed;
+	}
 
 }
--- differ-common/src/main/java/cz/nkp/differ/compare/io/GlitchDetectorResultPostProcessor.java
+++ differ-common/src/main/java/cz/nkp/differ/compare/io/GlitchDetectorResultPostProcessor.java
@@ -8,91 +8,112 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- *
+ * 
  * @author xrosecky
  */
-public class GlitchDetectorResultPostProcessor implements ResultPostProcessor, RefreshableResultPostProcessor<GlitchDetectorConfig> {
-    
-    public static final String SOURCE_NAME = "Glitch detector";
+public class GlitchDetectorResultPostProcessor implements ResultPostProcessor,
+		RefreshableResultPostProcessor<GlitchDetectorConfig> {
 
-    private static final MetadataSource source = new MetadataSource(SOURCE_NAME);
+	public static final String SOURCE_NAME = "Glitch detector";
 
-    private static final String PIXEL_UNIT = "px";
+	private static final MetadataSource source = new MetadataSource(SOURCE_NAME);
 
-    private static final String PERCENT_UNIT = "%";
+	private static final String PIXEL_UNIT = "px";
 
-    private static final String[] colours = new String[]{ "Max red ratio", "Max green ratio", "Max blue ratio" };
+	private static final String PERCENT_UNIT = "%";
 
-    private GlitchDetectorConfig config = new GlitchDetectorConfig();
+	private static final String[] colours = new String[] { "Max red ratio",
+			"Max green ratio", "Max blue ratio" };
 
-    @Override
-    public List<ImageMetadata> process(ImageProcessorResult result, GlitchDetectorConfig glitchDetectorConfig) {
-	List<ImageMetadata> metadata = new ArrayList<ImageMetadata>();
-	if (result.getType().equals(ImageProcessorResult.Type.COMPARISON)) {
-            return Collections.emptyList();
-        }
-        boolean glitchDetected = false;
-        int resolution = result.getHeight() * result.getWidth();
-        int[] grayscaleHistogram = result.getBlackAndWhiteHistogram();
-        if (grayscaleHistogram != null) {
-            boolean valid = true;
-            if (grayscaleHistogram[0] == resolution || grayscaleHistogram[255] == resolution ) {
-                glitchDetected = true;
-                valid = false;
-            }
-	    ImageMetadata white = createImageMetadataEntry("Number of absolute white pixels", Integer.toString(grayscaleHistogram[255]), valid, PIXEL_UNIT);
-	    metadata.add(white);
-	    ImageMetadata black = createImageMetadataEntry("Number of absolute black pixels", Integer.toString(grayscaleHistogram[0]), valid, PIXEL_UNIT);
-	    metadata.add(black);
-        }
-        int[][] colorHistogram = result.getHistogram();
-        if (colorHistogram != null) {
-            for (int i = 0; i != 3; i++) {
-                double ratio = colorHistogram[i][255] / (resolution * 1.0);
-                double maxAllowedRatio = 1.0;
-		switch (i) {
-		    case 0: maxAllowedRatio = glitchDetectorConfig.getMaxAllowedRatioForAbsoluteRed(); break;
-		    case 1: maxAllowedRatio = glitchDetectorConfig.getMaxAllowedRatioForAbsoluteGreen(); break;
-		    case 2: maxAllowedRatio = glitchDetectorConfig.getMaxAllowedRatioForAbsoluteBlue(); break;
+	private GlitchDetectorConfig config = new GlitchDetectorConfig();
+
+	@Override
+	public List<ImageMetadata> process(ImageProcessorResult result,
+			GlitchDetectorConfig glitchDetectorConfig) {
+		List<ImageMetadata> metadata = new ArrayList<ImageMetadata>();
+		if (result.getType().equals(ImageProcessorResult.Type.COMPARISON)) {
+			return Collections.emptyList();
 		}
-                boolean valid = true;
-                if (ratio > maxAllowedRatio) {
-                    glitchDetected = true;
-                    valid = false;
-                }
-		ImageMetadata entry = createImageMetadataEntry(colours[i], Double.toString(ratio * 100),  valid, PERCENT_UNIT);
+		boolean glitchDetected = false;
+		int resolution = result.getHeight() * result.getWidth();
+		int[] grayscaleHistogram = result.getBlackAndWhiteHistogram();
+		if (grayscaleHistogram != null) {
+			boolean valid = true;
+			if (grayscaleHistogram[0] == resolution
+					|| grayscaleHistogram[255] == resolution) {
+				glitchDetected = true;
+				valid = false;
+			}
+			ImageMetadata white = createImageMetadataEntry(
+					"Number of absolute white pixels",
+					Integer.toString(grayscaleHistogram[255]), valid,
+					PIXEL_UNIT);
+			metadata.add(white);
+			ImageMetadata black = createImageMetadataEntry(
+					"Number of absolute black pixels",
+					Integer.toString(grayscaleHistogram[0]), valid, PIXEL_UNIT);
+			metadata.add(black);
+		}
+		int[][] colorHistogram = result.getHistogram();
+		if (colorHistogram != null) {
+			for (int i = 0; i != 3; i++) {
+				double ratio = colorHistogram[i][255] / (resolution * 1.0);
+				double maxAllowedRatio = 1.0;
+				switch (i) {
+				case 0:
+					maxAllowedRatio = glitchDetectorConfig
+							.getMaxAllowedRatioForAbsoluteRed();
+					break;
+				case 1:
+					maxAllowedRatio = glitchDetectorConfig
+							.getMaxAllowedRatioForAbsoluteGreen();
+					break;
+				case 2:
+					maxAllowedRatio = glitchDetectorConfig
+							.getMaxAllowedRatioForAbsoluteBlue();
+					break;
+				}
+				boolean valid = true;
+				if (ratio > maxAllowedRatio) {
+					glitchDetected = true;
+					valid = false;
+				}
+				ImageMetadata entry = createImageMetadataEntry(colours[i],
+						Double.toString(ratio * 100), valid, PERCENT_UNIT);
+				metadata.add(entry);
+			}
+		}
+		ImageMetadata entry = new ImageMetadata();
+		entry.setSource(source);
+		entry.setKey("Glitch detected");
+		entry.setValue(new ValidatedProperty(Boolean.toString(glitchDetected),
+				glitchDetected));
 		metadata.add(entry);
-            }
-        }
-        ImageMetadata entry = new ImageMetadata();
-        entry.setSource(source);
-        entry.setKey("Glitch detected");
-        entry.setValue(new ValidatedProperty(Boolean.toString(glitchDetected), glitchDetected));
-	metadata.add(entry);
-	return metadata;
-    }
+		return metadata;
+	}
 
-    @Override
-    public void process(ImageProcessorResult result) {
-        result.getMetadata().addAll(process(result, config));
-    }
+	@Override
+	public void process(ImageProcessorResult result) {
+		result.getMetadata().addAll(process(result, config));
+	}
 
-    public GlitchDetectorConfig getConfig() {
-	return config;
-    }
+	public GlitchDetectorConfig getConfig() {
+		return config;
+	}
 
-    public void setConfig(GlitchDetectorConfig config) {
-	this.config = config;
-    }
-    
-    private ImageMetadata createImageMetadataEntry(String key, String value, boolean valid, String unit) {
-	ImageMetadata entry = new ImageMetadata();
-        entry.setSource(source);
-        entry.setKey(key);
-	entry.setUnit(unit);
-        ValidatedProperty property = new ValidatedProperty(value, valid);
-        entry.setValue(property);
-	return entry;
-    }
-    
+	public void setConfig(GlitchDetectorConfig config) {
+		this.config = config;
+	}
+
+	private ImageMetadata createImageMetadataEntry(String key, String value,
+			boolean valid, String unit) {
+		ImageMetadata entry = new ImageMetadata();
+		entry.setSource(source);
+		entry.setKey(key);
+		entry.setUnit(unit);
+		ValidatedProperty property = new ValidatedProperty(value, valid);
+		entry.setValue(property);
+		return entry;
+	}
+
 }
