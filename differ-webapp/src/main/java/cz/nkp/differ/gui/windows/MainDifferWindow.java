df5700f95592aad4dfd6e2093db475cfce1795c6
Vaclav Rosecky <xrosecky@gmail.com>
2014-03-09 10:15:05 +0100

    results tab fixed

--- differ-webapp/src/main/java/cz/nkp/differ/gui/windows/MainDifferWindow.java
+++ differ-webapp/src/main/java/cz/nkp/differ/gui/windows/MainDifferWindow.java
@@ -22,98 +22,114 @@ import java.util.List;
  * @author Joshua Mabrey
  * @author Thomas Truax
  * 
- * Sept. 2013
+ *         Sept. 2013
  */
 @SuppressWarnings("serial")
 public class MainDifferWindow extends Window implements LoginListener {
-    
-    private static final int POSITION_OF_RESULT_IN_TABS = 2;
 
-    private List<UserFilesWidget> userFilesWidgets;
-    private TabSheet menuTabs;
-    private Tab resultsTab = null;
-    
-    public MainDifferWindow() {
-	super("The Image Data Validator - DIFFER");//Sets the title of the application
+	private static final int POSITION_OF_RESULT_IN_TABS = 2;
 
-	menuTabs = new TabSheet();
+	private List<UserFilesWidget> userFilesWidgets;
+	private TabSheet menuTabs;
+	private Tab resultsTab = null;
 
-	/*
-	 * Adding the dynamic content tabs
-	 */
-        DifferProgramTab loginContext = new DifferProgramTab(this);
-	MainDifferWindow.createDynamicContentTab(loginContext, "DIFFER", menuTabs);
-        
-        ResultManagerTab examples = new ResultManagerTab(this, true);
-        MainDifferWindow.createDynamicContentTab(examples, "Examples", menuTabs);
-        
-        if (DifferApplication.getCurrentApplication().getLoggedUser() != null) {
-            ResultManagerTab results = new ResultManagerTab(this, false);
-            resultsTab = MainDifferWindow.createDynamicContentTab(results, "Results", menuTabs);
-        }
-        
-	MainDifferWindow.createStaticContentTab("doc_tab", "Documentation", menuTabs);
-	MainDifferWindow.createStaticContentTab("about_tab", "About", menuTabs);
-        MainDifferWindow.createStaticContentTab("traffic_tab", "Traffic", menuTabs);
+	public MainDifferWindow() {
+		super("The Image Data Validator - DIFFER");// Sets the title of the
+													// application
 
-	/*
-	 * Add the actual completed UI components to the root
-	 */
-	addComponent(new ProjectHeaderPanel(this, loginContext));//Component that represents the top-page header
-	addComponent(menuTabs);//The application view tabs
-        addComponent(new ProjectFooterComponent());
-    }
-    
-    public void init() {
-        DifferApplication.getCurrentApplication().addLoginListener(this);
-    }
+		menuTabs = new TabSheet();
 
-    private static Tab createStaticContentTab(String source, String caption, TabSheet parent) {
-	try {
-	    VerticalLayout tab = new TabLoader(source);
-	    tab.setMargin(true);
-	    tab.setCaption(caption);
-	    return parent.addTab(tab);
-	} catch (IOException e) {
-	    e.printStackTrace();
+		/*
+		 * Adding the dynamic content tabs
+		 */
+		DifferProgramTab loginContext = new DifferProgramTab(this);
+		MainDifferWindow.createDynamicContentTab(loginContext, "DIFFER",
+				menuTabs);
+
+		ResultManagerTab examples = new ResultManagerTab(this, true);
+		MainDifferWindow
+				.createDynamicContentTab(examples, "Examples", menuTabs);
+
+		if (DifferApplication.getCurrentApplication().getLoggedUser() != null) {
+			ResultManagerTab results = new ResultManagerTab(this, false);
+			resultsTab = MainDifferWindow.createDynamicContentTab(results,
+					"Results", menuTabs);
+		}
+
+		MainDifferWindow.createStaticContentTab("doc_tab", "Documentation",
+				menuTabs);
+		MainDifferWindow.createStaticContentTab("about_tab", "About", menuTabs);
+		MainDifferWindow.createStaticContentTab("traffic_tab", "Traffic",
+				menuTabs);
+
+		/*
+		 * Add the actual completed UI components to the root
+		 */
+		addComponent(new ProjectHeaderPanel(this, loginContext));// Component
+																	// that
+																	// represents
+																	// the
+																	// top-page
+																	// header
+		addComponent(menuTabs);// The application view tabs
+		addComponent(new ProjectFooterComponent());
 	}
-        return null;
-    }
 
-    private static Tab createDynamicContentTab(Layout source, String caption, TabSheet parent) {
-	source.setCaption(caption);
-	source.setMargin(true);
-	return parent.addTab(source);
-    }
-    
-    private static Tab createDynamicContentTab(Layout source, String caption, TabSheet parent, int position) {
-	source.setCaption(caption);
-	source.setMargin(true);
-	return parent.addTab(source, position);
-    }
+	public void init() {
+		DifferApplication.getCurrentApplication().addLoginListener(this);
+	}
 
-    public List<UserFilesWidget> getUserFilesWidgets() {
-	return userFilesWidgets;
-    }
+	private static Tab createStaticContentTab(String source, String caption,
+			TabSheet parent) {
+		try {
+			VerticalLayout tab = new TabLoader(source);
+			tab.setMargin(true);
+			tab.setCaption(caption);
+			return parent.addTab(tab);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-    public void setUserFilesWidgets(List<UserFilesWidget> userFilesWidgets) {
-	this.userFilesWidgets = userFilesWidgets;
-    }
+	private static Tab createDynamicContentTab(Layout source, String caption,
+			TabSheet parent) {
+		source.setCaption(caption);
+		source.setMargin(true);
+		return parent.addTab(source);
+	}
 
-    @Override
-    public void onLogin(User user) {
-        if (resultsTab == null) {
-            ResultManagerTab results = new ResultManagerTab(this, false);
-            resultsTab = MainDifferWindow.createDynamicContentTab(results, "Results", menuTabs, POSITION_OF_RESULT_IN_TABS);
-        }
-    }
+	private static Tab createDynamicContentTab(Layout source, String caption,
+			TabSheet parent, int position) {
+		source.setCaption(caption);
+		source.setMargin(true);
+		return parent.addTab(source, position);
+	}
 
-    @Override
-    public void onLogout(User user) {
-        if (resultsTab != null) {
-            menuTabs.removeTab(resultsTab);
-            resultsTab = null;
-        }
-    }
-    
+	public List<UserFilesWidget> getUserFilesWidgets() {
+		return userFilesWidgets;
+	}
+
+	public void setUserFilesWidgets(List<UserFilesWidget> userFilesWidgets) {
+		this.userFilesWidgets = userFilesWidgets;
+	}
+
+	@Override
+	public void onLogin(User user) {
+		if (resultsTab != null) {
+			menuTabs.removeTab(resultsTab);
+		}
+		ResultManagerTab results = new ResultManagerTab(this, false);
+		resultsTab = MainDifferWindow.createDynamicContentTab(results,
+				"Results", menuTabs, POSITION_OF_RESULT_IN_TABS);
+	}
+
+	@Override
+	public void onLogout(User user) {
+		if (resultsTab != null) {
+			menuTabs.removeTab(resultsTab);
+			resultsTab = null;
+		}
+	}
+
 }
